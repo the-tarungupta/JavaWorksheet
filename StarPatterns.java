2b3a5be21219081f18d1a096d82700ae768a2452
Tarun Gupta <168983071+the-tarungupta@users.noreply.github.com>
2024-12-14 14:16:24 +0530

    contains star patterns programs

---
+++ StarPatterns.java
@@ -0,0 +1,99 @@
+class StarPatterns{
+    public static void main(String[] args){
+
+        int n = 4;        // no. of rows
+
+        
+        // Pattern 1
+        /*
+                *
+                **
+                ***
+                ****
+        */
+
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        // Pattern 2
+        /*
+                ****
+                ***
+                **
+                *
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=n; j>=i; j--){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+       
+
+
+       // Pattern 3
+       /*
+                *
+                **
+                ***
+                ****
+                ***
+                **
+                *
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+    //    for(int i=1; i<=n-1; i++){
+    //     for(int j=n-1; j>=i; j--){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        // Pattern 4
+        /* 
+                ****
+                ***
+                **
+                *
+                **
+                ***
+                ****
+        */
+
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=n; j>=i; j--){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+    //    for(int i=1; i<=n-1; i++){
+    //     for(int j=0; j<=i; j++){
+    //         System.out.print("*");
+    //     }
+    //      System.out.println();
+    //    }
+
+
+    }
+}
