d81484600e1e90b3b141e1f0f15f2f0a7e8c0417
Tarun Gupta <168983071+the-tarungupta@users.noreply.github.com>
2024-12-14 17:33:24 +0530

    added star patterns programs

--- StarPatterns.java
+++ StarPatterns.java
@@ -95,5 +95,118 @@ class StarPatterns{
     //    }
 
 
+
+        // Pattern 5
+        /*
+                    *
+                   **
+                  ***
+                 ****
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=n-1; j>=i; j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=1; k<=i; k++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+        // Pattern 6
+        /*
+                ****
+                 ***
+                  **
+                   *
+        */
+
+       
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=2; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=n; k>=i; k--){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        // Pattern 7
+        /* 
+                    *
+                   **
+                  ***
+                 ****
+                  ***
+                   **
+                    *
+        */
+
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=n-1; j>=i; j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=1; k<=i; k++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+    //    for(int i=1; i<=n-1; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=n-1; k>=i; k--){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        // Pattern 8
+        /*
+                ****
+                 ***
+                  **
+                   *
+                  **
+                 ***
+                ****
+        */
+
+
+       
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=2; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=n; k>=i; k--){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+    //    for(int i=1; i<=n-1; i++){
+    //     for(int j=2; j>=i; j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=0; k<=i; k++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //    }
+       
+
+
+    
+
+
     }
 }
