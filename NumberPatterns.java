91541d9a6b6e242457634fa4006c401dcfa2525d
Tarun Gupta <168983071+the-tarungupta@users.noreply.github.com>
2024-12-16 13:44:43 +0530

    contains number patterns programs

---
+++ NumberPatterns.java
@@ -0,0 +1,104 @@
+class NumberPatterns{
+    public static void main(String[] args){
+
+        int n=4;
+
+        // Pattern 1
+        /*
+             1
+             1 2
+             1 2 3
+             1 2 3 4
+        */
+
+
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+        // Pattern 2
+        /*
+             1
+             2 2
+             3 3 3
+             4 4 4 4
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(i+" ");
+    //     }
+    //     System.out.println();
+    //    } 
+
+
+
+        // Pattern 3
+        /*
+             1 2 3 4
+             1 2 3 
+             1 2
+             1 
+        */
+
+       
+    //    for(int i=n; i>=1; i--){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(j+" ");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+        // Pattern 4
+        /*
+             4 4 4 4
+             3 3 3
+             2 2 
+             1
+        */
+
+
+
+    //    for(int i=n; i>=1; i--){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(i+" ");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        // Pattern 5
+        /*
+             1 
+             2 3 
+             4 5 6
+             7 8 9 10
+        */
+
+    //    int var = 1;
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(var+" ");
+    //         var++;
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        
+
+        
+
+
+              
+    }
+}
