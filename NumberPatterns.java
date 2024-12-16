d9ccfa9fb67243333ac33d29e770fd4535f4b2ba
Tarun Gupta <168983071+the-tarungupta@users.noreply.github.com>
2024-12-16 23:32:13 +0530

    added number patterns programs

--- NumberPatterns.java
+++ NumberPatterns.java
@@ -94,7 +94,46 @@ class NumberPatterns{
 
 
 
-        
+        // Pattern 6
+        /*
+                1
+               2 2
+              3 3 3
+            4  4  4  4 
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=n-1; j>=i; j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=1; k<=i; k++){
+    //         System.out.print(i+" ");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+        // Pattern 7
+        /*
+                1 2 3 4                    
+                 2 3 4
+                  3 4
+                   4
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=2; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=i; k<=n; k++){
+    //         System.out.print(k+" ");
+    //     }
+    //     System.out.println(); 
+    //    }
+
+
 
         
 
