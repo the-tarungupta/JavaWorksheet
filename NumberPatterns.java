79f2149acec6b5756040ff3390ff64d407bb9ed2
Tarun Gupta <168983071+the-tarungupta@users.noreply.github.com>
2024-12-17 12:41:10 +0530

    added number patterns programs

--- NumberPatterns.java
+++ NumberPatterns.java
@@ -135,6 +135,86 @@ class NumberPatterns{
 
 
 
+        // Pattern 8
+        /*
+                 1 2 3 4 
+                  2 3 4
+                   3 4
+                    4
+                   3 4
+                  2 3 4
+                 1 2 3 4
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=2; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=i; k<=n; k++){
+    //         System.out.print(k+" ");
+    //     }
+    //     System.out.println();
+    //    }
+    //    for(int i=n-1; i>=1; i--){
+    //     for(int j=n-2; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=i; k<=n; k++){
+    //         System.out.print(k+" ");
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        // Pattern 9
+        /*
+             1 
+             0 1
+             1 0 1
+             0 1 0 1
+        */
+
+       
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         if((i+j)%2==0){
+    //             System.out.print(1+" ");
+    //         }
+    //         else{
+    //             System.out.print(0+" ");
+    //         }
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+        // Pattern 10
+        /*
+            0
+            1 0
+            0 1 0
+            1 0 1 0
+        */
+
+
+    //    for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         if(((i+j)%2==0)){
+    //             System.out.print(0+" ");
+    //         }
+    //         else{
+    //             System.out.print(1+" ");
+    //         }
+    //     }
+    //     System.out.println();
+    //    }
+
+
+
+
         
 
 
